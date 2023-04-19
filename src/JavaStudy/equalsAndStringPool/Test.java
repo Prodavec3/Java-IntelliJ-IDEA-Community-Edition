fb8ba715c945f32fb3a3b96d73bd9d24a3237970
Prodavec3 <destroedst@mail.ru>
2023-04-19 17:46:56 +0300

    new

--- src/JavaStudy/equalsAndStringPool/Test.java
+++ src/JavaStudy/equalsAndStringPool/Test.java
@@ -2,6 +2,20 @@ package JavaStudy.equalsAndStringPool;
 
 public class Test {
     public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+        String str3 = "Hello123".substring(0, 5); // 0 это H, 5 это 1, метод режет строку, правая граница не включитель
+        System.out.println("Сравнение через ==: "+ str2 == str3); // false, т.к мы сравниваем не через equals, ссылки на объекты разные
+        /**
+         * нам выдаст сразу true, хотя при сравнении объектов было false
+         * Оно рабоатет потому что есть StringPool - строка помещается в пул и указатель ссылается на эту строку
+         * если второй раз мы создает такую же строку, то java ссылает указатель str2 на тот же объект, т.е ссылки str1=str2
+         * т.о java экономит память и проверяет есть такая строка в пуле или нет
+         *
+         * Однако если String str1 = new String() создаем таким образом - StringPool срабатывать не будет => str1 != str2
+         */
+        System.out.println("Сравнение одинаковых строк: " + str1.equals(str2));
+
         int x = 1;
         int y = 2;
         System.out.println(x == y); //true, также double и тд
