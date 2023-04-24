6ced336796b109ffdbfd95eb77c9530692a9b07f
Prodavec3 <destroedst@mail.ru>
2023-04-24 09:56:10 +0300

    add recurs, enum, end base course

---
+++ src/JavaStudy/Recursiya/Test.java
@@ -0,0 +1,31 @@
+package JavaStudy.Recursiya;
+
+/**
+ * Вызов метода в теле этого же метода
+ */
+public class Test {
+    public static void main(String[] args) {
+        //someMethod();
+        counter(10);
+    }
+
+    private static void someMethod(){
+        System.out.println("Hello");
+        someMethod();
+    } // StackOverFlow выдаст ошибку, т.к метод будет работать бесконечно
+
+    /**
+     * Информация о вызовах методов хранится в стеке.
+     * размер памяти под стек выделяется ограниченное количество
+     *
+     * чтобы рекурсия была полезна - необходимо условие выхода
+     */
+
+    private static void counter(int n){
+        if (n == 0){
+            return;
+        }
+        System.out.println("Число: "+n);
+        counter(n-1); // рекурсия ждет последующих выполнений итераций
+    }
+}
