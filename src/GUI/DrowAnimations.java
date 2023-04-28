ae77a72b1c7a5103af7fc05bb9bd4dec2e524e91
Prodavec3 <destroedst@mail.ru>
2023-04-28 16:14:20 +0300

    add gui Animations

---
+++ src/GUI/DrowAnimations.java
@@ -0,0 +1,51 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrowAnimations {
+    int x, y = 70;
+
+    public static void main(String[] args) {
+        DrowAnimations gui = new DrowAnimations();
+        gui.go();
+    }
+
+    public void go(){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(drawPanel);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+
+        for (int i = 0; i < 200; i++){
+            x++;
+            y++;
+
+            drawPanel.repaint();
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    class MyDrawPanel extends JPanel{
+        public void paintComponent(Graphics g){
+            /**
+             * Для анимации небходимо менять координаты объекта (х и у у нас увеличиваются и так,
+             * и необходимо также стирать старое положение - закрашивать белым цветом
+             */
+            g.setColor(Color.WHITE);
+            g.fillRect(0,0,this.getWidth(), this.getHeight());
+
+            g.setColor(Color.GREEN);
+            g.fillOval(x,y,40,40);
+        }
+    }
+}
---
+++ src/GUI/TwoButtons.java
@@ -0,0 +1,64 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * 2 кнопки делают разные действия
+ * чтобы следить за 2мя разными кнопками реализуем 2 класса, которые будут от интерфейса ActionListener
+ *
+ * Внутренние классы позволяют реализовать один и тот же интерфейс в классе.
+ *
+ * Для реализации 3х кнопок надо будет 3 класса с интерфейсом ActionListener
+ */
+
+public class TwoButtons {
+    JFrame frame;
+    JLabel label;
+
+    public static void main(String[] args) {
+        TwoButtons gui = new TwoButtons();
+        gui.go();
+    }
+
+    public void go(){
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JButton labelButton = new JButton("Изменить надпись");
+        labelButton.addActionListener(new LabelListener());
+
+        JButton colorButton = new JButton("Изменить цвет");
+        colorButton.addActionListener(new ColorListener());
+
+        label = new JLabel("Текст");
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.getContentPane().add(BorderLayout.EAST, labelButton);
+        frame.getContentPane().add(BorderLayout.WEST, label);
+
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    class LabelListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            label.setText("Нажал на кнопку");
+        }
+    }
+
+    class ColorListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            frame.repaint();
+        }
+    }
+
+}
